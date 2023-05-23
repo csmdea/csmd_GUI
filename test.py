40ffe178602ffcf796dbc67cff223e6563e8fbee
kimminse3380 <mss2214@naver.com>
2023-05-23 11:56:16 +0900

    add::full screen and ESC event

--- test.py
+++ test.py
@@ -1,24 +1,29 @@
 import tkinter
+import sys
 
-window=tkinter.Tk()
+window = tkinter.Tk()
 
 window.title("TEST")
-window.geometry("1240x1240+100+100")
-window.resizable(False, False)
-
+window.attributes("-fullscreen", True)
 window['bg'] = '#FFFFFF'
 
-count=0
+count = 0
 
 def countUP():
     global count
-    count +=1
+    count += 1
     label.config(text=str(count))
 
-label=tkinter.Label(window, text="파이썬", width=10, height=5, fg="red", relief="solid")
+def exitApp(event):
+    sys.exit()
+
+label = tkinter.Label(window, text="파이썬", width=10, height=5, fg="red", relief="solid")
 label.pack()
 
-button = tkinter.Button(window, text="click",overrelief="solid", width=15, command=countUP, repeatdelay=1000, repeatinterval=100)
+button = tkinter.Button(window, text="click", overrelief="solid", width=15, command=countUP, repeatdelay=1000, repeatinterval=100)
 button.pack()
+
+# 'ESC' 키를 눌렀을 때 exitApp 함수가 호출되도록 바인딩합니다.
+window.bind('<Escape>', exitApp)
 
 window.mainloop()
